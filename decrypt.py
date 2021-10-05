80ee00d8b47cb71b63dbef571e961b0dcabb1437
apoorva <apoorva.srivastava2019@vitstudent.ac.in>
2021-10-05 22:32:12 +0530

    removed useless comments

--- decrypt.py
+++ decrypt.py
@@ -21,30 +21,20 @@ def main():
     private_key = data["private_key"]
     file_type = data["file_type"]
     # This is on the receiver side
-
-    ######################################################################################
     # Decrypt with ECC to get the AES key
     ecc_AESkey = ECC.ECC()
     decryptedAESkey = ecc_AESkey.decryption(C1_aesKey, C2_aesKey, private_key)
-    ######################################################################################
 
     C1_multimedia = data["C1_multimedia"]
     C2_multimedia = data["C2_multimedia"]
-    ######################################################################################
     # 1. Decrypt the data with ECC
     ecc_obj = ECC.ECC()
     encrypted_multimedia = ecc_obj.decryption(C1_multimedia, C2_multimedia, private_key)
     clean_data_list = converter.makeListFromString(encrypted_multimedia)
-    ######################################################################################
-
-    ######################################################################################
     # 2. Decrypt with AES
     # aes_multimedia_data = AES.AES(int(hex(int(decryptedAESkey)),0))
     aes_obj = AES.AES(int(decryptedAESkey))
     decrypted_multimedia = aes_obj.decryptBigData(clean_data_list)
-    ######################################################################################
-
-    ######################################################################################
     # 3. Decode from Base64 to the corresponding fileToBase64
     output_file = "test."+file_type
     converter.base64ToFile(decrypted_multimedia, output_file)
@@ -52,7 +42,6 @@ def main():
     if(delete):
         import os
         os.remove(output_file)
-    ######################################################################################
 
 
 if __name__ == "__main__":
--- encrypt.py
+++ encrypt.py
@@ -21,7 +21,6 @@ def main():
 
     # im = Image.open("test_files/"+input_file)
     # width, height = im.size
-    ######################################################################################
     # 1. Multimedia data -> Base64 Encoding and plain text
     multimedia_data = converter.fileToBase64("test_files/" + input_file)
 
@@ -29,33 +28,24 @@ def main():
 
     # multimedia_data = (b"hey").decode('utf-8')
     # multimedia_data = input("Enter the data >")
-    ######################################################################################
 
     aes_key = 57811460909138771071931939740208549692
 
-    ######################################################################################
     # Encrypt  AES_key with ECC public key
     ecc_obj_AESkey = ECC.ECC()
     private_key = 59450895769729158456103083586342075745962357150281762902433455229297926354304
     public_key = ecc_obj_AESkey.gen_pubKey(private_key)
     (C1_aesKey, C2_aesKey) = ecc_obj_AESkey.encryption(public_key, str(aes_key))
     # decryptedAESkey = ecc_AESkey.decryption(C1_aesKey,C2_aesKey, private_key)
-    ######################################################################################
-
-    ######################################################################################
     # 2. Encrypt the multimedia_data with AES algorithm
     aes = AES.AES(aes_key)
     encrypted_multimedia = aes.encryptBigData(multimedia_data)
     data_for_ecc = converter.makeSingleString(encrypted_multimedia)
 
     # converter.base64ToFile(decrypted_multimedia.encode('utf-8'),"test_success.jpg")
-    ######################################################################################
-
-    ######################################################################################
     # 3. Encrypt the encrypted_multimedia with ECC
     ecc = ECC.ECC()
     (C1_multimedia, C2_multimedia) = ecc.encryption(public_key, data_for_ecc)
-    ######################################################################################
 
     cipher = {
         "file_type": file_type,
